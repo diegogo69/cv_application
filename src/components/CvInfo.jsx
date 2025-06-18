01f7e63de5d1c004ab7c20ef36a7a439a34d65c0
diegogo69 <153313506+diegogo69@users.noreply.github.com>
2025-06-17 22:02:11 -0400

    feat: add function handler to form action

--- src/components/CvInfo.jsx
+++ src/components/CvInfo.jsx
@@ -5,15 +5,15 @@ function GeneralInfo() {
       <ul>
         <li>
           <label htmlFor="name">Name:</label>
-          <input type="text" id="name" />
+          <input type="text" name="name" id="name" />
         </li>
         <li>
           <label htmlFor="email">Email:</label>
-          <input type="email" id="email" />
+          <input type="email" name="email" id="email" />
         </li>
         <li>
           <label htmlFor="tel">Phone number:</label>
-          <input type="tel" id="tel" />
+          <input type="tel" name="tel" id="tel" />
         </li>
       </ul>
     </section>
@@ -27,59 +27,67 @@ function EducationalInfo() {
       <ul>
         <li>
           <label htmlFor="school">School name:</label>
-          <input type="text" id="school" />
+          <input type="text" name="school" id="school" />
         </li>
         <li>
           <label htmlFor="study-title">Study title:</label>
-          <input type="text" id="study-title" />
+          <input type="text" name="study-title" id="study-title" />
         </li>
         <li>
           <label htmlFor="study-date-from">From:</label>
-          <input type="date" id="study-date-from" />
+          <input type="date" name="study-date-from" id="study-date-from" />
         </li>
         <li>
           <label htmlFor="study-date-to">To:</label>
-          <input type="date" id="study-date-to" />
+          <input type="date" name="study-date-to" id="study-date-to" />
         </li>
       </ul>
     </section>
-  )
+  );
 }
 
 function PracticalExpInfo() {
   return (
     <section>
       <h3>Practical Experience</h3>
-      {/* company name, position title,
-      main responsibilities of your jobs,
-      date from and until when you worked */}
       <ul>
         <li>
           <label htmlFor="company-name">Company name:</label>
-          <input type="text" id="company-name" />
+          <input type="text" name="company-name" id="company-name" />
         </li>
         <li>
           <label htmlFor="positition-title">Positition title:</label>
-          <input type="text" id="positition-title" />
+          <input type="text" name="positition-title" id="positition-title" />
         </li>
         <li>
           <label htmlFor="position-date-from">From:</label>
-          <input type="date" id="position-date-from" />
+          <input
+            type="date"
+            name="position-date-from"
+            id="position-date-from"
+          />
         </li>
         <li>
           <label htmlFor="position-date-to">To:</label>
-          <input type="date" id="position-date-to" />
+          <input type="date" name="position-date-to" id="position-date-to" />
         </li>
       </ul>
     </section>
-  )
+  );
 }
+
+function handleSubmit(formData) {
+  console.log([...formData.keys()]);
+  console.log([...formData.values()]);
+}
+
 export default function CvInfo() {
   return (
-    <>
+    <form action={handleSubmit}>
       <GeneralInfo />
       <EducationalInfo />
       <PracticalExpInfo />
-    </>
+      <button type="submit">Submit</button>
+    </form>
   );
 }
