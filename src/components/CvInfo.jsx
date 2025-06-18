a07c8bea9c88f43d5aeb40ada1f5f18972f6ec08
diegogo69 <153313506+diegogo69@users.noreply.github.com>
2025-06-17 22:33:18 -0400

    feat: add useId's id to input fields and labels

--- src/components/CvInfo.jsx
+++ src/components/CvInfo.jsx
@@ -1,19 +1,28 @@
+import { useId } from "react";
+
 function GeneralInfo() {
+  const id = useId();
+  const ids = {
+    name: id + "-name",
+    email: id + "-email",
+    tel: id + "-tel",
+  };
+
   return (
     <section>
       <h3>General</h3>
       <ul>
         <li>
-          <label htmlFor="name">Name:</label>
-          <input type="text" name="name" id="name" />
+          <label htmlFor={ids.name}>Name:</label>
+          <input type="text" name="name" id={ids.name} />
         </li>
         <li>
-          <label htmlFor="email">Email:</label>
-          <input type="email" name="email" id="email" />
+          <label htmlFor={ids.email}>Email:</label>
+          <input type="email" name="email" id={ids.email} />
         </li>
         <li>
-          <label htmlFor="tel">Phone number:</label>
-          <input type="tel" name="tel" id="tel" />
+          <label htmlFor={ids.tel}>Phone number:</label>
+          <input type="tel" name="tel" id={ids.tel} />
         </li>
       </ul>
     </section>
@@ -21,25 +30,32 @@ function GeneralInfo() {
 }
 
 function EducationalInfo() {
+  const id = useId();
+  const ids = {
+    school: id + "-school",
+    studyTitle: id + "-study-title",
+    studyDateFrom: id + "-study-date-from",
+    studyDateTo: id + "-study-date-to",
+  };
   return (
     <section>
       <h3>Educational Info</h3>
       <ul>
         <li>
-          <label htmlFor="school">School name:</label>
-          <input type="text" name="school" id="school" />
+          <label htmlFor={ids.school}>School name:</label>
+          <input type="text" name="school" id={ids.school} />
         </li>
         <li>
-          <label htmlFor="study-title">Study title:</label>
-          <input type="text" name="study-title" id="study-title" />
+          <label htmlFor={ids.studyTitle}>Study title:</label>
+          <input type="text" name="study-title" id={ids.studyTitle} />
         </li>
         <li>
-          <label htmlFor="study-date-from">From:</label>
-          <input type="date" name="study-date-from" id="study-date-from" />
+          <label htmlFor={ids.studyDateFrom}>From:</label>
+          <input type="date" name="study-date-from" id={ids.studyDateFrom} />
         </li>
         <li>
-          <label htmlFor="study-date-to">To:</label>
-          <input type="date" name="study-date-to" id="study-date-to" />
+          <label htmlFor={ids.studyDateTo}>To:</label>
+          <input type="date" name="study-date-to" id={ids.studyDateTo} />
         </li>
       </ul>
     </section>
@@ -47,29 +63,37 @@ function EducationalInfo() {
 }
 
 function PracticalExpInfo() {
+  const id = useId();
+  const ids = {
+    companyName: id + "-company-name",
+    positionTitle: id + "-position-title",
+    positionDateFrom: id + "-position-date-from",
+    positionDateTo: id + "-position-date-to",
+  };
+
   return (
     <section>
       <h3>Practical Experience</h3>
       <ul>
         <li>
-          <label htmlFor="company-name">Company name:</label>
-          <input type="text" name="company-name" id="company-name" />
+          <label htmlFor={ids.companyName}>Company name:</label>
+          <input type="text" name="company-name" id={ids.companyName} />
         </li>
         <li>
-          <label htmlFor="positition-title">Positition title:</label>
-          <input type="text" name="positition-title" id="positition-title" />
+          <label htmlFor={ids.positionTitle}>Position title:</label>
+          <input type="text" name="position-title" id={ids.positionTitle} />
         </li>
         <li>
-          <label htmlFor="position-date-from">From:</label>
+          <label htmlFor={ids.positionDateFrom}>From:</label>
           <input
             type="date"
             name="position-date-from"
-            id="position-date-from"
+            id={ids.positionDateFrom}
           />
         </li>
         <li>
-          <label htmlFor="position-date-to">To:</label>
-          <input type="date" name="position-date-to" id="position-date-to" />
+          <label htmlFor={ids.positionDateTo}>To:</label>
+          <input type="date" name="position-date-to" id={ids.positionDateTo} />
         </li>
       </ul>
     </section>
