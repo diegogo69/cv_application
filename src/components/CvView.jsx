e99962a98e27ae027fc7e9aa9930c45ccffa7e51
diegogo69 <153313506+diegogo69@users.noreply.github.com>
2025-06-18 21:05:27 -0400

    feat: divide cv-view sections into components

--- src/components/CvView.jsx
+++ src/components/CvView.jsx
@@ -16,6 +16,41 @@ import "../styles/cv-view.css";
 ];
 // {Object.entries(cvData).map(([key, value]) => [key, value])}
 
+function CvGeneralItem({ cvData }) {
+  return (
+    <ul>
+      <li>{cvData.email || "user@contact.com"}</li>
+      <li>{cvData.tel || "555-5555-555"}</li>
+    </ul>
+  );
+}
+
+function CvEducationItem({ cvData }) {
+  return (
+    <ul>
+      <li>{cvData["school-title"] || "School title"}</li>
+      <li>{cvData.school || "School"}</li>
+      <li>
+        {cvData["study-date-from"] || "Jun, 2025"} -{" "}
+        {cvData["study-date-to"] || "present"}
+      </li>
+    </ul>
+  );
+}
+
+function CvExperienceItem({ cvData }) {
+  return (
+    <ul>
+      <li>{cvData["company-name"] || "Company name"}</li>
+      <li>{cvData["position-title"] || "Position title"}</li>
+      <li>{cvData["postion-resp"] || "Main responsabilities"}</li>
+      <li>
+        {cvData["position-date-from"] || "Jun, 2025"} -{" "}
+        {cvData["position-date-to"] || "present"}
+      </li>
+    </ul>
+  );
+}
 
 export default function CvView({ cvData }) {
   console.log(cvData);
@@ -25,33 +60,15 @@ export default function CvView({ cvData }) {
       <div className="cv-view">
         <section className="cv-general">
           <h1>{cvData.name || "Your name goes here"}</h1>
-          <ul>
-            <li>{cvData.email || "user@contact.com"}</li>
-            <li>{cvData.tel || "555-5555-555"}</li>
-          </ul>
+          <CvGeneralItem cvData={cvData} />
         </section>
         <section className="cv-educational">
           <h2>Education</h2>
-          <ul>
-            <li>{cvData["school-title"] || "School title"}</li>
-            <li>{cvData.school || "School"}</li>
-            <li>
-              {cvData["study-date-from"] || "Jun, 2025"} -{" "}
-              {cvData["study-date-to"] || "present"}
-            </li>
-          </ul>
+          <CvEducationItem cvData={cvData} />
         </section>
         <section className="cv-practical">
           <h2>Experience</h2>
-          <ul>
-            <li>{cvData["company-name"] || "Company name"}</li>
-            <li>{cvData["position-title"] || "Position title"}</li>
-            <li>{cvData["postion-resp"] || "Main responsabilities"}</li>
-            <li>
-              {cvData["position-date-from"] || "Jun, 2025"} -{" "}
-              {cvData["position-date-to"] || "present"}
-            </li>
-          </ul>
+          <CvExperienceItem cvData={cvData} />
         </section>
       </div>
     </div>
