2af1b071283185fdce91d2a561d9270727b1f84a
diegogo69 <153313506+diegogo69@users.noreply.github.com>
2025-06-25 09:51:09 -0400

    feat: add edit and remove item icons

--- src/components/formSections.jsx
+++ src/components/formSections.jsx
@@ -45,6 +45,18 @@ function EducationalInfo({
   eduItems,
   setFormData,
 }) {
+  const EDIT_ITEM_SVG = (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+      <title>Edit</title>
+      <path d="M5,3C3.89,3 3,3.89 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V12H19V19H5V5H12V3H5M17.78,4C17.61,4 17.43,4.07 17.3,4.2L16.08,5.41L18.58,7.91L19.8,6.7C20.06,6.44 20.06,6 19.8,5.75L18.25,4.2C18.12,4.07 17.95,4 17.78,4M15.37,6.12L8,13.5V16H10.5L17.87,8.62L15.37,6.12Z" />
+    </svg>
+  );
+  const REM_ITEM_SVG = (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+      <title>Remove</title>
+      <path d="M9,3V4H4V6H5V19A2,2 0 0,0 7,21H17A2,2 0 0,0 19,19V6H20V4H15V3H9M7,6H17V19H7V6M9,8V17H11V8H9M13,8V17H15V8H13Z" />
+    </svg>
+  );
   const [errMsgs, setErrMsgs] = useState({});
   const [defaultValues, setDefaultValues] = useState(cvData);
   const [editItem, setEditItem] = useState({});
@@ -109,7 +121,7 @@ function EducationalInfo({
   const cancelSaveEditEdu = (e) => {
     e.preventDefault();
     setEditItem({});
-  }
+  };
   const handleSaveEditEdu = (e) => {
     e.preventDefault();
     saveEditEduItem(e, editItem.index);
@@ -171,26 +183,26 @@ function EducationalInfo({
               className="info-item"
               onClick={(e) => selectEduItem(e, index, eduItem.key)}
             >
-              <header>
-                {eduItem["study-title"]}
+              <header>{eduItem["study-title"]}</header>
+              <div>{eduItem["school"]}</div>
+              <div>
+                {eduItem["study-date-from"]} - {eduItem["study-date-to"]}
+              </div>
+              <footer>
                 <div className="item-btns">
                   <button
                     type="button"
                     onClick={(e) => handleEditEdu(e, index, eduItem.key)}
                   >
-                    Edit
+                    {EDIT_ITEM_SVG}
                   </button>
                   <button
                     type="button"
                     onClick={(e) => handleRemoveEdu(e, index, eduItem.key)}
                   >
-                    Rem
+                    {REM_ITEM_SVG}
                   </button>
                 </div>
-              </header>
-              <div>{eduItem["school"]}</div>
-              <footer>
-                {eduItem["study-date-from"]} - {eduItem["study-date-to"]}
               </footer>
             </div>
           );
